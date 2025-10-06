2877c0468891d04d00833ad767910222c9a27b63
VictorDemerji <demerji.v1@gmail.com>
2025-10-06 17:43:32 +0300

    Create constructors

--- src/Produs.java
+++ src/Produs.java
@@ -4,4 +4,9 @@ public class Produs {
     double pret;
     int cantitate;
 
+    public Produs(String nume, double pret, int cantitate) {
+        this.nume      = nume;
+        this.pret      = pret;
+        this.cantitate = cantitate;
+    }
 }

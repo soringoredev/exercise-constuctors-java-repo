6043a02b351c3d2fd245d169601b38065eba0625
Mija Cristian <mija.cristian3101@gmail.com>
2025-10-06 18:05:23 +0300

    Add method calculeazaTotalAchitare in class Produs

--- src/Produs.java
+++ src/Produs.java
@@ -3,10 +3,18 @@ public class Produs {
     String nume;
     double pret;
     int cantitate;
+    double total;
 
-    public Produs(String nume, double pret, int cantitate) {
+    Produs(String nume, double pret, int cantitate) {
         this.nume      = nume;
         this.pret      = pret;
         this.cantitate = cantitate;
+    }
+
+    public double calculeazaTotalAchitare() {
+        Produs  prod1 = new Produs("Lapte",20,1);
+        total = cantitate * pret;
+       return total;
+
     }
 }
--- src/ProdusRun.java
+++ src/ProdusRun.java
@@ -1,2 +1,6 @@
 public class ProdusRun {
+    public static void main(String[] args) {
+        Produs total1 = new Produs("Lapte",20,1 );
+        System.out.println("Achitarea totala este de: " + total1.calculeazaTotalAchitare() +" mdl.");
+    }
 }
